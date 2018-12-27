c95a15d8f55c7985573f7a7e44673c8652f2cc76
Евгений Фуреев <efureev@yandex.ru>
2018-12-28 01:16:00 +0500

    add File Params

--- src/Entities/FileParams.php
+++ src/Entities/FileParams.php
@@ -1,15 +1,238 @@
 <?php
 
-namespace Feugene\Files\Support;
+namespace Feugene\Files\Entities;
+
+
+use Illuminate\Contracts\Support\Arrayable;
+use Php\Support\Exceptions\InvalidParamException;
+use Php\Support\Exceptions\UnknownPropertyException;
+use Php\Support\Helpers\Json;
+use Php\Support\Interfaces\Jsonable;
 
 /**
  * Class FileParams
  *
  * @package Feugene\Files\Support
- * @codeCoverageIgnore
  */
-class FileParams
+class FileParams implements
+    \ArrayAccess,
+    \IteratorAggregate,
+    \JsonSerializable,
+    \Countable,
+    Arrayable,
+    Jsonable
 {
+    /** @var array */
+    protected $_items = [];
 
 
+    /**
+     * Params constructor.
+     *
+     * @param array|null $array
+     */
+    public function __construct(?array $array = [])
+    {
+        $this->init();
+        $this->fromArray($array ?? []);
+    }
+
+    /**
+     * Initialization component
+     */
+    public function init()
+    {
+    }
+
+    /**
+     * @return array
+     */
+    public function jsonSerialize(): array
+    {
+        return array_map(function ($value) {
+            if ($value instanceof \JsonSerializable) {
+                return $value->jsonSerialize();
+            } elseif ($value instanceof Jsonable) {
+                return Json::decode($value->toJson(), true);
+            } elseif ($value instanceof Arrayable) {
+                return $value->toArray();
+            }
+
+            return $value;
+        }, $this->_items);
+    }
+
+    /**
+     * Get items as JSON
+     *
+     * @param int $options
+     *
+     * @return string|null
+     */
+    public function toJson($options = 320): ?string
+    {
+        return Json::encode($this->jsonSerialize(), $options);
+    }
+
+    /**
+     * @param string $string
+     *
+     * @return \Php\Support\Interfaces\Jsonable|\Php\Support\Components\Params
+     */
+    public static function fromJson(string $string): ?Jsonable
+    {
+        return new static(Json::decode($string));
+    }
+
+    /**
+     * Convert items to its string representation.
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->toJson() ?? '[]';
+    }
+
+
+    /**
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return Json::dataToArray($this->_items);
+    }
+
+    /**
+     * @param array $array
+     *
+     * @return $this
+     */
+    public function fromArray(array $array)
+    {
+        $this->_items = $array;
+
+        return $this;
+    }
+
+    /**
+     * Clear elements
+     *
+     * @return $this
+     */
+    public function clear()
+    {
+        $this->_items = [];
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function count(): int
+    {
+        return count($this->_items);
+    }
+
+    /**
+     * Checks if the given key or index exists in the array
+     *
+     * @param mixed $offset
+     *
+     * @return bool
+     */
+    public function offsetExists($offset): bool
+    {
+        return array_key_exists($offset, $this->_items);
+    }
+
+    /**
+     * Offset to retrieve
+     *
+     * @param mixed $offset
+     *
+     * @return mixed
+     */
+    public function offsetGet($offset)
+    {
+        return $this->_items[ $offset ];
+    }
+
+    /**
+     * Offset to set
+     *
+     * @param mixed $offset The offset to assign the value to.
+     * @param mixed $value  The value to set.
+     */
+    public function offsetSet($offset, $value)
+    {
+        if (is_null($offset)) {
+            throw new InvalidParamException('$offset must have not be null');
+        } else {
+            $this->_items[ $offset ] = $value;
+        }
+    }
+
+    /**
+     * Offset to unset
+     *
+     * @param mixed $offset The offset to unset.
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->_items[ $offset ]);
+    }
+
+    /**
+     * @return \ArrayIterator|\Traversable
+     */
+    public function getIterator()
+    {
+        return new \ArrayIterator($this->_items);
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return mixed
+     * @throws \Php\Support\Exceptions\UnknownPropertyException
+     */
+    public function __get(string $name)
+    {
+        if ($this->offsetExists($name)) {
+            return $this->offsetGet($name);
+        }
+
+        throw new UnknownPropertyException('Getting unknown property: ' . get_class($this) . '::' . $name, $name);
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return bool
+     */
+    public function __isset(string $name)
+    {
+        return $this->offsetExists($name);
+    }
+
+    /**
+     * @param string $name
+     * @param mixed  $value
+     */
+    public function __set(string $name, $value)
+    {
+        $this->offsetSet($name, $value);
+    }
+
+    /**
+     * @param string $name
+     */
+    public function __unset(string $name)
+    {
+        if ($this->offsetExists($name)) {
+            $this->offsetUnset($name);
+        }
+    }
 }
--- tests/Unit/Models/FileTest.php
+++ tests/Unit/Models/FileTest.php
@@ -2,6 +2,7 @@
 
 namespace Feugene\Files\Tests\Unit\Models;
 
+use Feugene\Files\Entities\FileParams;
 use Feugene\Files\Models\File;
 use Feugene\Files\Support\Store;
 use Feugene\Files\Traits\BaseFileApply;
@@ -85,6 +86,106 @@ class FileTest extends AbstractModelTestCase
         static::assertInstanceOf(File::class, $file);
     }
 
+    public function testLoadFromDatabaseWithParams(): void
+    {
+        $file = File::find($this->model->getKey());
+
+        static::assertInstanceOf(FileParams::class, $file->params);
+
+        $file->params->width = 21;
+        $file->params->height = 100;
+
+        static::assertEquals(21, $file->params->width);
+        static::assertEquals(100, $file->params->height);
+        static::assertIsArray($file->params->toArray());
+        static::assertEquals(["width" => 21, "height" => 100], $file->params->toArray());
+        static::assertEquals('{"width":21,"height":100}', $file->params->toJson());
+        static::assertJson($file->params->toJson());
+        static::assertJsonStringEqualsJsonString('{"width":21,"height":100}', $file->params->toJson());
+
+        $file->save();
+
+        $file = File::find($this->model->getKey());
+        static::assertJsonStringEqualsJsonString('{"width":21,"height":100}', $file->getOriginal('params'));
+
+        static::assertEquals(21, $file->params->width);
+        static::assertEquals(100, $file->params->height);
+        static::assertIsArray($file->params->toArray());
+        static::assertEquals(["width" => 21, "height" => 100], $file->params->toArray());
+        static::assertEquals('{"width":21,"height":100}', $file->params->toJson());
+        static::assertJson($file->params->toJson());
+        static::assertJsonStringEqualsJsonString('{"width":21,"height":100}', $file->params->toJson());
+
+        $file->params->clear();
+        $file->save();
+
+        $file = File::find($this->model->getKey());
+        static::assertJsonStringEqualsJsonString('[]', $file->getOriginal('params'));
+
+        static::assertIsArray($file->params->toArray());
+        static::assertEquals([], $file->params->toArray());
+        static::assertEquals('[]', $file->params->toJson());
+        static::assertJson($file->params->toJson());
+        static::assertJsonStringEqualsJsonString('[]', $file->params->toJson());
+
+        $file->params->size = 3000;
+        $file->params->status = true;
+        $file->params->message = 'success';
+
+        $file->save();
+
+        $file = File::find($this->model->getKey());
+        static::assertJson($file->params->toJson());
+        static::assertIsArray($file->params->toArray());
+
+        static::assertEquals(["size" => 3000, "status" => true, "message" => "success"], $file->params->toArray());
+        static::assertEquals('{"size":3000,"status":true,"message":"success"}', $file->params->toJson());
+        static::assertCount(3, $file->params);
+
+        foreach ($file->params as $name => $value) {
+            switch ($name) {
+                case 'size':
+                    static::assertEquals(3000, $value);
+                    break;
+                case 'status':
+                    static::assertEquals(true, $value);
+                    break;
+                case 'message':
+                    static::assertEquals('success', $value);
+                    break;
+            }
+        }
+
+        unset($file->params->size);
+
+        static::assertEquals(["status" => true, "message" => "success"], $file->params->toArray());
+        static::assertEquals('{"status":true,"message":"success"}', $file->params->toJson());
+
+        $file->params->offsetUnset('status');
+        static::assertEquals(["message" => "success"], $file->params->toArray());
+        static::assertEquals('{"message":"success"}', $file->params->toJson());
+
+        static::assertTrue(isset($file->params->message));
+    }
+
+    /**
+     * @expectedException \Php\Support\Exceptions\UnknownPropertyException
+     */
+    public function testLoadFromDatabaseWithParamsFail(): void
+    {
+        $file = File::find($this->model->getKey());
+        $file->params->width;
+    }
+
+    /**
+     * @expectedException \Php\Support\Exceptions\InvalidParamException
+     */
+    public function testLoadFromDatabaseWithParamsFail_2(): void
+    {
+        $file = File::find($this->model->getKey());
+        $file->params->offsetSet(null, 12);
+    }
+
     /**
      * @throws \Exception
      */
@@ -152,7 +253,6 @@ class FileTest extends AbstractModelTestCase
     }
 
 
-
     /**
      * @expectedException \Feugene\Files\Exceptions\MissingFilePathException
      */
@@ -180,5 +280,6 @@ class FileTest extends AbstractModelTestCase
         $file = new File;
         $file->path = '';
     }
+
 
 }
